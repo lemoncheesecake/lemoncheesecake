bc5b02957f8e2891605f93ba097e25506aad7303
Nicolas Delon <nicolas.delon@gmail.com>
2016-04-15 20:21:40 +0200

    add offline HTML reports option

--- lemoncheesecake/reportingbackends/html.py
+++ lemoncheesecake/reportingbackends/html.py
@@ -8,16 +8,23 @@ import sys
 import re
 import os
 import os.path as p
-from shutil import copy
+from shutil import copy, copytree
 
 from lemoncheesecake.reporting import ReportingBackend
+
+OFFLINE_MODE = False
 
 class HtmlBackend(ReportingBackend):
     def begin_tests(self):
         html_resource_dir = p.join(p.dirname(__file__), p.pardir, "resources", "html")
-        
         report_resource_dir = p.join(self.report_dir, ".html") 
         
         os.mkdir(report_resource_dir)
-        copy(p.join(html_resource_dir, "report.html"), self.report_dir)
         copy(p.join(html_resource_dir, "report.js"), report_resource_dir)
+        
+        if OFFLINE_MODE:
+            copy(p.join(html_resource_dir, "report_offline.html"), p.join(self.report_dir, "report.html"))
+            copy(p.join(html_resource_dir, "jquery-1.12.3.min.js"), report_resource_dir)
+            copytree(p.join(html_resource_dir, "bootstrap-3.3.6-dist"), p.join(report_resource_dir, "bootstrap-3.3.6-dist"))
+        else:
+            copy(p.join(html_resource_dir, "report.html"), self.report_dir)
--- myproject/settings.py
+++ myproject/settings.py
@@ -3,10 +3,13 @@
 import sys
 sys.path.insert(0, ".")
 
+from lemoncheesecake.reportingbackends import html
 from lemoncheesecake.common import reporting_dir_with_datetime
 
 from test import *
 TESTSUITES = [ MyTestSuite, MyTestSuite1 ]
+
+html.OFFLINE_MODE = True
 
 # Reporting
 REPORTING_ROOT_DIR = "reports"
