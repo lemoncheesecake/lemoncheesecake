d356f67af24cf3576e04faf863d3902c264f8855
Nicolas Delon <nicolas.delon@gmail.com>
2016-06-12 17:12:30 +0200

    Remove obsolete code

--- myproject/settings.py
+++ myproject/settings.py
@@ -3,7 +3,6 @@
 import sys
 sys.path.insert(0, ".")
 
-from lemoncheesecake.reportingbackends import html
 from lemoncheesecake.common import reporting_dir_with_datetime
 from lemoncheesecake.loader import load_testsuites_from_directory
 from lemoncheesecake.reportingbackends.console import ConsoleBackend
@@ -11,12 +10,7 @@ from lemoncheesecake.reportingbackends.xml import XmlBackend
 from lemoncheesecake.reportingbackends.json_ import JsonBackend
 from lemoncheesecake.reportingbackends.html import HtmlBackend
 
-from test import *
-
-TESTSUITES = load_testsuites_from_directory("suites") #[ MyTestSuite, MyTestSuite1 ]
-# TESTSUITES = [ "MyTestSuite" ]
-
-html.OFFLINE_MODE = True
+TESTSUITES = load_testsuites_from_directory("suites")
 
 # Reporting
 REPORTING_ROOT_DIR = "reports"
