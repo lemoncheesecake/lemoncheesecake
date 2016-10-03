c12b7eb7db4ba96c05b99a78051ea482a0289cf9
Nicolas Delon <nicolas.delon@gmail.com>
2016-10-03 14:17:31 +0200

    Add module API doc

--- lemoncheesecake/__init__.py
+++ lemoncheesecake/__init__.py
@@ -1,6 +1,15 @@
+"""
+A module testsuite can safely import this module using a wild import. The content
+of the following modules will be directly accessible:
+- lemoncheesecake.testsuite
+- lemoncheesecake.runtime
+- lemoncheesecake.checkers
+"""
+
 from lemoncheesecake.testsuite import *
 from lemoncheesecake.runtime import *
 from lemoncheesecake.checkers import *
+from lemoncheesecake.exceptions import AbortTest, AbortTestSuite, AbortAllTests
 from lemoncheesecake.worker import Worker
 
 worker = None
