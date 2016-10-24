27fba61d56ad750f129bc2e9ad4d3bd1aa6a073c
Nicolas Delon <nicolas.delon@gmail.com>
2016-10-25 00:44:10 +0200

    unit tests: improve test coverage for decorators applied to TestSuite classes

--- tests/test_testsuite.py
+++ tests/test_testsuite.py
@@ -9,7 +9,7 @@ import pytest
 import lemoncheesecake as lcc
 from lemoncheesecake.exceptions import ProgrammingError
 
-def test_testsuite_decorator_test():
+def test_decorator_test():
     class MySuite(lcc.TestSuite):
         @lcc.test("test_desc")
         def mytest(self):
@@ -20,7 +20,7 @@ def test_testsuite_decorator_test():
     assert test.id == "mytest"
     assert test.description == "test_desc"
 
-def test_testsuite_decorator_tags():
+def test_decorator_tags():
     @lcc.tags("tag3")
     class MySuite(lcc.TestSuite):
         @lcc.tags("tag1", "tag2")
@@ -29,11 +29,11 @@ def test_testsuite_decorator_tags():
             pass
     suite = MySuite()
     suite.load()
-    assert suite.tags == ("tag3",)
+    assert suite.tags == ["tag3"]
     test = suite.get_tests()[0]
-    assert test.tags == ("tag1", "tag2")
+    assert test.tags == ["tag1", "tag2"]
 
-def test_testsuite_decorator_prop():
+def test_decorator_prop():
     @lcc.prop("key1", "val1")
     class MySuite(lcc.TestSuite):
         @lcc.prop("key2", "val2")
@@ -46,7 +46,7 @@ def test_testsuite_decorator_prop():
     test = suite.get_tests()[0]
     assert test.properties["key2"] == "val2"
 
-def test_testsuite_decorator_url():
+def test_decorator_url():
     @lcc.url("http://www.example.com", "example")
     class MySuite(lcc.TestSuite):
         @lcc.url("http://www.example.com")
@@ -59,8 +59,41 @@ def test_testsuite_decorator_url():
     test = suite.get_tests()[0]
     assert test.urls[0] == ("http://www.example.com", None)
 
-def test_testsuite_decorator_on_invalid_object():
+def test_decorator_on_invalid_object():
     with pytest.raises(ProgrammingError):
         @lcc.tags("tag")
         class NotATestSuite:
             pass
+
+def test_decorator_with_testsuite_inheritance():
+    @lcc.url("http://www.example1.com")
+    @lcc.tags("tag1", "tag2")
+    @lcc.prop("key1", "value1")
+    class MySuite1(lcc.TestSuite):
+        pass
+    
+    @lcc.url("http://www.example2.com")
+    @lcc.tags("tag3")
+    @lcc.prop("key2", "value2")
+    class MySuite2(MySuite1):
+        pass
+    
+    suite1 = MySuite1()
+    suite1.load()
+    suite2 = MySuite2()
+    suite2.load()
+    
+    assert len(suite1.urls) == 1
+    assert suite1.urls[0] == ("http://www.example1.com", None)
+    assert suite1.tags == ["tag1", "tag2"]
+    assert len(suite1.properties) == 1
+    assert suite1.properties["key1"] == "value1"
+    
+    assert len(suite2.urls) == 2
+    assert suite2.urls[0] == ("http://www.example1.com", None)
+    assert suite2.urls[1] == ("http://www.example2.com", None)
+    assert suite2.tags == ["tag1", "tag2", "tag3"]
+    assert len(suite2.properties) == 2
+    assert suite2.properties["key1"] == "value1"
+    assert suite2.properties["key2"] == "value2"
+    
