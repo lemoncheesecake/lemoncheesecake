089793f31ed6922ed30946a343ee6ec74d1b9547
Nicolas Delon <nicolas.delon@gmail.com>
2016-10-31 14:44:01 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "lemoncheesecake",
-    version = "0.1.0",
+    version = "0.2.0",
     packages = find_packages(),
     include_package_data = True,
     install_requires = ["colorama", "termcolor", "lxml"]
