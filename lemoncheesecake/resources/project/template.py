d5425e4cd8aa8db8702eaf69aa53270725206d79
Nicolas Delon <nicolas.delon@gmail.com>
2017-01-06 23:11:32 +0100

    Minor improvements in project template

--- lemoncheesecake/resources/project/template.py
+++ lemoncheesecake/resources/project/template.py
@@ -1,11 +1,14 @@
 import os.path
 
 from lemoncheesecake import loader
-from lemoncheesecake.reporting import backend, reportdir
+from lemoncheesecake.reporting import backend, backends, reportdir
 from lemoncheesecake import validators
 
+# Variables
+project_dir = os.path.dirname(__file__)
+
 # Test suites and workers
-TESTSUITES = loader.import_testsuites_from_directory(os.path.join(os.path.dirname(__file__), "tests"))
+TESTSUITES = loader.import_testsuites_from_directory(os.path.join(project_dir, "tests"))
 WORKERS = {}
 def add_cli_args(cli_parser):
     pass
@@ -16,8 +19,13 @@ mp = validators.MetadataPolicy()
 METADATA_POLICY = mp
 
 # Reporting
-REPORTING_BACKENDS = backend.get_available_backends()
-REPORTING_BACKENDS_ACTIVE = "console", "json", "html"
+console_backend = backends.ConsoleBackend()
+json_backend = backends.JsonBackend()
+xml_backend = backends.XmlBackend()
+html_backend = backends.HtmlBackend()
+
+REPORTING_BACKENDS = console_backend, json_backend, xml_backend, html_backend
+REPORTING_BACKENDS_ACTIVE = console_backend.name, json_backend.name, xml_backend.name
 REPORT_DIR_CREATION = lambda top_dir: reportdir.report_dir_with_archiving(top_dir, reportdir.archive_dirname_datetime)
 
 # Test run hooks
