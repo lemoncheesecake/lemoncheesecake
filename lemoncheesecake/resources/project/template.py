bbb1fa4d39c113721257d4cb3ee5564d83f02143
Nicolas Delon <nicolas.delon@gmail.com>
2017-01-29 22:00:05 +0100

    Reorganize import directives

--- lemoncheesecake/resources/project/template.py
+++ lemoncheesecake/resources/project/template.py
@@ -1,33 +1,34 @@
 import os.path
 
-from lemoncheesecake import loader
-from lemoncheesecake.reporting import backend, backends, reportdir
-from lemoncheesecake import validators
+from lemoncheesecake.loader import import_testsuites_from_directory, import_fixtures_from_directory
+from lemoncheesecake.reporting.backends import ConsoleBackend, JsonBackend, XmlBackend, HtmlBackend
+from lemoncheesecake.reporting.reportdir import report_dir_with_archiving, archive_dirname_datetime
+from lemoncheesecake.validators import MetadataPolicy
 
 # Variables
 project_dir = os.path.dirname(__file__)
 
 # Test suites and workers
-TESTSUITES = loader.import_testsuites_from_directory(os.path.join(project_dir, "tests"))
-FIXTURES = loader.import_fixtures_from_directory(os.path.join(project_dir, "fixtures"))
+TESTSUITES = import_testsuites_from_directory(os.path.join(project_dir, "tests"))
+FIXTURES = import_fixtures_from_directory(os.path.join(project_dir, "fixtures"))
 WORKERS = {}
 def add_cli_args(cli_parser):
     pass
 CLI_EXTRA_ARGS = add_cli_args
 
 # Metadata policy
-mp = validators.MetadataPolicy()
+mp = MetadataPolicy()
 METADATA_POLICY = mp
 
 # Reporting
-console_backend = backends.ConsoleBackend()
-json_backend = backends.JsonBackend()
-xml_backend = backends.XmlBackend()
-html_backend = backends.HtmlBackend()
+console_backend = ConsoleBackend()
+json_backend = JsonBackend()
+xml_backend = XmlBackend()
+html_backend = HtmlBackend()
 
 REPORTING_BACKENDS = console_backend, json_backend, xml_backend, html_backend
 REPORTING_BACKENDS_ACTIVE = console_backend.name, json_backend.name, html_backend.name
-REPORT_DIR_CREATION = lambda top_dir: reportdir.report_dir_with_archiving(top_dir, reportdir.archive_dirname_datetime)
+REPORT_DIR_CREATION = lambda top_dir: report_dir_with_archiving(top_dir, archive_dirname_datetime)
 
 # Test run hooks
 def before_tests(report_dir):
