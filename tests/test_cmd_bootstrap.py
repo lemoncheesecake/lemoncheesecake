205e3acbc0901bdf1c1641c1c80561cacd7a9664
Nicolas Delon <nicolas.delon@gmail.com>
2017-02-25 17:09:56 +0100

    Add unit tests for the 'bootstrap' command

---
+++ tests/test_cmd_bootstrap.py
@@ -0,0 +1,18 @@
+import os.path as osp
+
+from helpers import cmdout
+
+from lemoncheesecake.cli import main
+from lemoncheesecake.project import Project
+
+def test_bootstrap(tmpdir):
+    project_dir = tmpdir.join("myproj").strpath
+    assert main(["bootstrap", project_dir]) == 0
+    
+    project = Project(osp.join(project_dir, "project.py"))
+    assert project.get_testsuites_classes() == []
+
+def test_bootstrap_existing_directory(tmpdir):
+    out = main(["bootstrap", tmpdir.strpath])
+    
+    assert out.startswith("Cannot create project")
