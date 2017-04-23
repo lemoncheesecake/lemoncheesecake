bea482eafe3cb46074d5b50dc4295c44e8466175
Nicolas Delon <nicolas.delon@gmail.com>
2017-04-23 18:47:25 +0200

    Add a 'quiet' mode for matching operations

--- lemoncheesecake/matching/operations.py
+++ lemoncheesecake/matching/operations.py
@@ -9,25 +9,25 @@ from lemoncheesecake.exceptions import AbortTest
 
 __all__ = ("log_match_result", "check_that", "require_that", "assert_that")
 
-def log_match_result(hint, matcher, result):
+def log_match_result(hint, matcher, result, quiet=False):
     description = "Expect %s %s" % (hint, matcher.description())
     return get_runtime().check(
-        description, result.outcome, str(result.description) if result.description != None else None
+        description, result.outcome, str(result.description) if not quiet and result.description != None else None
     )
 
-def check_that(hint, actual, matcher):
+def check_that(hint, actual, matcher, quiet=False):
     result = matcher.matches(actual)
-    log_match_result(hint, matcher, result)
+    log_match_result(hint, matcher, result, quiet=quiet)
     return result.outcome
 
-def require_that(hint, actual, matcher):
+def require_that(hint, actual, matcher, quiet=False):
     result = matcher.matches(actual)
-    log_match_result(hint, matcher, result)
+    log_match_result(hint, matcher, result, quiet=quiet)
     if result.is_failure():
         raise AbortTest("previous requirement was not fulfilled")
 
-def assert_that(hint, actual, matcher):
+def assert_that(hint, actual, matcher, quiet=False):
     result = matcher.matches(actual)
     if result.is_failure():
-        log_match_result(hint, matcher, result)
+        log_match_result(hint, matcher, result, quiet=quiet)
         raise AbortTest("assertion error")
