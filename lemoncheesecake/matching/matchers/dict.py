ec1c2f03e5c64c6d8096f46a0732d6397fd3e857
Nicolas Delon <nicolas.delon@gmail.com>
2017-05-14 21:06:58 +0200

    Refactoring existing 'has_entry' matcher using new EntryMatcher interface

--- lemoncheesecake/matching/matchers/dict.py
+++ lemoncheesecake/matching/matchers/dict.py
@@ -4,33 +4,59 @@ Created on Apr 1, 2017
 @author: nicolas
 '''
 
-from lemoncheesecake.matching.base import Matcher, match_failure, match_success, got, to_have
+from lemoncheesecake.exceptions import method_not_implemented
+from lemoncheesecake.matching.base import Matcher, match_failure, match_success, got_value, to_have
 from lemoncheesecake.matching.matchers.composites import is_
 
 __all__ = (
     "has_entry",
 )
 
-class HasEntry(Matcher):
-    def __init__(self, key, value_matcher):
+class EntryMatcher:
+    def description(self):
+        method_not_implemented("description", self)
+    
+    def get_entry(self, actual):
+        """Return the value of dict corresponding to entry matching or raise KeyError if entry is not found""" 
+        method_not_implemented("get_entry", self)
+
+class KeyMatcher(EntryMatcher):
+    def __init__(self, key):
         self.key = key
+    
+    def description(self):
+        return '"%s"' % self.key
+    
+    def get_entry(self, actual):
+        return actual[self.key]
+
+def wrap_key_matcher(key_matcher):
+    if isinstance(key_matcher, EntryMatcher):
+        return key_matcher
+    return KeyMatcher(key_matcher)
+
+class HasEntry(Matcher):
+    def __init__(self, key_matcher, value_matcher):
+        self.key_matcher = key_matcher
         self.value_matcher = value_matcher
     
     def description(self, conjugate=False):
-        ret = '%s entry "%s"' % (to_have(conjugate), self.key)
+        ret = '%s entry %s' % (to_have(conjugate), self.key_matcher.description())
         if self.value_matcher:
             ret += " that " + self.value_matcher.description(conjugate=True)
         return ret
     
     def matches(self, actual):
-        if self.key in actual:
-            if self.value_matcher:
-                return self.value_matcher.matches(actual[self.key])
-            else:
-                return match_success(got(actual[self.key]))
+        try:
+            value = self.key_matcher.get_entry(actual)
+        except KeyError:
+            return match_failure('No entry "%s"' % self.key_matcher.description())
+        
+        if self.value_matcher:
+            return self.value_matcher.matches(value)
         else:
-            return match_failure('No entry "%s"' % self.key)
+            return match_success(got_value(value))
 
-def has_entry(key, value_matcher=None):
+def has_entry(key_matcher, value_matcher=None):
     """Test if dict has a <key> entry whose value matches (optional) value_matcher""" 
-    return HasEntry(key, is_(value_matcher) if value_matcher != None else None)
+    return HasEntry(wrap_key_matcher(key_matcher), is_(value_matcher) if value_matcher != None else None)
