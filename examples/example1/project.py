ed9031bb4dc5d9bef2f54e7d80314650877a4205
Nicolas Delon <nicolas.delon@gmail.com>
2017-06-01 22:39:24 +0200

    example1: enable SAVE_AT_EACH_EVENT save mode in order to test HTML report in a 'in-progress' mode

--- examples/example1/project.py
+++ examples/example1/project.py
@@ -3,6 +3,8 @@ import os
 from lemoncheesecake.testsuite import load_testsuites_from_directory
 from lemoncheesecake.fixtures import load_fixtures_from_directory
 from lemoncheesecake.reporting import reportdir
+from lemoncheesecake.reporting.backend import SAVE_AT_EACH_EVENT
+from lemoncheesecake.reporting.backends import ConsoleBackend, JsonBackend, XmlBackend, HtmlBackend
 
 project_dir = os.path.dirname(__file__)
 TESTSUITES = load_testsuites_from_directory("suites")
@@ -11,6 +13,17 @@ FIXTURES = load_fixtures_from_directory(os.path.join(project_dir, "fixtures"))
 def fail(dummy):
     raise Exception("operation has failed")
 
+###
+# Reporting
+###
+console_backend = ConsoleBackend()
+json_backend = JsonBackend()
+json_backend.save_mode = SAVE_AT_EACH_EVENT
+xml_backend = XmlBackend()
+html_backend = HtmlBackend()
+
+REPORTING_BACKENDS = console_backend, json_backend, xml_backend, html_backend
+REPORTING_BACKENDS_ACTIVE = console_backend.name, json_backend.name, html_backend.name
 REPORT_DIR_CREATION = lambda top_dir: reportdir.report_dir_with_archiving(top_dir, reportdir.archive_dirname_datetime)
 
 # Test run hooks
