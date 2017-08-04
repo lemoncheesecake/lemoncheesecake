c654cf851c937319964acb57876760d19360c39a
Nicolas Delon <nicolas.delon@gmail.com>
2017-08-04 22:49:52 +0200

    Rename lemoncheesecake.matching.matchers.types into ...types_ to avoid conflicts with types Python module

--- lemoncheesecake/matching/matchers/__init__.py
+++ lemoncheesecake/matching/matchers/__init__.py
@@ -2,7 +2,7 @@ from .value import *
 from .string import *
 from .list_ import *
 from .dict_ import *
-from .types import *
+from .types_ import *
 from .composites import *
 
 __all__ = list(filter(lambda sym: not sym.startswith("_"), dir()))
