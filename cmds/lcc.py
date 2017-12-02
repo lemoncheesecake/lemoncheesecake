bde1795a9193ed2d61a0918de943a565677332c0
Nicolas Delon <nicolas.delon@gmail.com>
2017-12-02 18:16:57 +0100

    lcc.py: use working directory lemoncheesecake over any installed lemoncheesecake

--- cmds/lcc.py
+++ cmds/lcc.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 from lemoncheesecake.cli import main
 
