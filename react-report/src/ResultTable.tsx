98a1e338a7f688aef95d3865ac1d5b7ac97fc433
Nicolas Delon <nicolas.delon@gmail.com>
2017-12-25 22:53:26 +0100

    HTML report: make more room for check result details

--- react-report/src/ResultTable.tsx
+++ react-report/src/ResultTable.tsx
@@ -18,9 +18,9 @@ class ResultTable extends React.Component<Props, {}> {
                 <table className="table table-hover table-bordered table-condensed">
                     <colgroup>
                         <col style={{"width": "10%"}}/>
-                        <col style={{"width": "60%"}}/>
-                        <col style={{"width": "20%"}}/>
-                        <col style={{"width": "10%"}}/>
+                        <col style={{"width": "50%"}}/>
+                        <col style={{"width": "25%"}}/>
+                        <col style={{"width": "15%"}}/>
                     </colgroup>
                     { this.props.children }
                 </table>
