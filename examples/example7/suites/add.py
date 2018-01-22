0eb27f6afb02861f6b20bd60d3029bcbd2e2a938
Nicolas Delon <nicolas.delon@gmail.com>
2018-01-22 22:35:40 +0100

    Add example7 test project for data-driven test demo

---
+++ examples/example7/suites/add.py
@@ -0,0 +1,23 @@
+import os.path as osp
+
+import json
+
+import lemoncheesecake.api as lcc
+from lemoncheesecake.matching import *
+
+project_dir = osp.join(osp.dirname(__file__), "..")
+
+
+def test_add(i, j, expected):
+    check_that("%d + %d" % (i, j), i + j, equal_to(expected))
+
+
+@lcc.suite("Add")
+class add:
+    def __init__(self):
+        data = json.load(open(osp.join(project_dir, "data.json")))
+        for entry in data:
+            lcc.add_test_in_suite(
+                lcc.Test(entry["name"], entry["description"], lambda _: test_add(entry["i"], entry["j"], entry["expected"])),
+                self
+            )
