73107b11765ab9ca9f15327a3d86ca791394c132
Nicolas Delon <nicolas.delon@gmail.com>
2018-02-03 14:10:37 +0100

    test_matching_operations: code cleanup

--- tests/test_matching_operations.py
+++ tests/test_matching_operations.py
@@ -6,16 +6,16 @@ Created on Dec 1, 2016
 @author: nicolas
 '''
 
-import lemoncheesecake.api as lcc
+from lemoncheesecake.matching import *
 
 from helpers.runner import run_func_in_test
-from helpers.report import get_last_logged_check, assert_last_test_status, get_last_test, \
+from helpers.report import get_last_logged_check, get_last_test_checks, assert_last_test_status, get_last_test, \
     count_logs, assert_test_checks
 
 
 def test_check_that_success():
     check = get_last_logged_check(
-        run_func_in_test(lambda: lcc.check_that("value", "foo", lcc.equal_to("foo")))
+        run_func_in_test(lambda: check_that("value", "foo", equal_to("foo")))
     )
 
     assert "value" in check.description and "foo" in check.description
@@ -25,7 +25,7 @@ def test_check_that_success():
 
 def test_check_that_failure():
     check = get_last_logged_check(
-        run_func_in_test(lambda: lcc.check_that("value", "bar", lcc.equal_to("foo")))
+        run_func_in_test(lambda: check_that("value", "bar", equal_to("foo")))
     )
 
     assert "value" in check.description and "foo" in check.description
@@ -35,7 +35,7 @@ def test_check_that_failure():
 
 def test_check_that_entry():
     check = get_last_logged_check(
-        run_func_in_test(lambda: lcc.check_that_entry("foo", lcc.equal_to("bar"), in_={"foo": "bar"}))
+        run_func_in_test(lambda: check_that_entry("foo", equal_to("bar"), in_={"foo": "bar"}))
     )
 
     assert "foo" in check.description and "bar" in check.description
@@ -47,7 +47,7 @@ def test_require_that_success():
     marker = []
     def test():
         marker.append("before_test")
-        lcc.require_that("value", "foo", lcc.equal_to("foo"))
+        require_that("value", "foo", equal_to("foo"))
         marker.append("after_test")
     check = get_last_logged_check(run_func_in_test(test))
 
@@ -61,7 +61,7 @@ def test_require_that_failure():
     marker = []
     def test():
         marker.append("before_test")
-        lcc.require_that("value", "bar", lcc.equal_to("foo"))
+        require_that("value", "bar", equal_to("foo"))
         marker.append("after_test")
     report = run_func_in_test(test)
     check = get_last_logged_check(report)
@@ -74,7 +74,7 @@ def test_require_that_failure():
 
 def test_require_that_entry():
     check = get_last_logged_check(
-        run_func_in_test(lambda: lcc.require_that_entry("foo", lcc.equal_to("bar"), in_={"foo": "bar"}))
+        run_func_in_test(lambda: require_that_entry("foo", equal_to("bar"), in_={"foo": "bar"}))
     )
 
     assert "foo" in check.description and "bar" in check.description
@@ -83,7 +83,7 @@ def test_require_that_entry():
 
 
 def test_assert_that_success():
-    report = run_func_in_test(lambda: lcc.assert_that("value", "foo", lcc.equal_to("foo")))
+    report = run_func_in_test(lambda: assert_that("value", "foo", equal_to("foo")))
     assert_last_test_status(report, "passed")
 
 
@@ -91,7 +91,7 @@ def test_assert_that_failure():
     marker = []
     def test():
         marker.append("before_test")
-        lcc.assert_that("value", "bar", lcc.equal_to("foo"))
+        assert_that("value", "bar", equal_to("foo"))
         marker.append("after_test")
     report = run_func_in_test(test)
     check = get_last_logged_check(report)
@@ -104,20 +104,20 @@ def test_assert_that_failure():
 
 
 def test_assert_that_entry_success():
-    report = run_func_in_test(lambda: lcc.assert_that_entry("foo", lcc.equal_to("bar"), in_={"foo": "bar"}))
+    report = run_func_in_test(lambda: assert_that_entry("foo", equal_to("bar"), in_={"foo": "bar"}))
     test = get_last_test(report)
     assert len(test.steps) == 0
 
 
 def test_assert_that_entry_failure():
-    report = run_func_in_test(lambda: lcc.assert_that_entry("foo", lcc.equal_to("bar"), in_={"foo": "baz"}))
+    report = run_func_in_test(lambda: assert_that_entry("foo", equal_to("bar"), in_={"foo": "baz"}))
     assert_last_test_status(report, "failed")
 
 
 def test_this_dict():
     def func():
-        with lcc.this_dict({"foo": "bar"}):
-            lcc.check_that_entry("foo", lcc.equal_to("bar"))
+        with this_dict({"foo": "bar"}):
+            check_that_entry("foo", equal_to("bar"))
 
     check = get_last_logged_check(run_func_in_test(func))
 
@@ -128,10 +128,10 @@ def test_this_dict():
 
 def test_this_dict_multiple():
     def func():
-        with lcc.this_dict({"foo": "bar"}):
-            lcc.check_that_entry("foo", lcc.equal_to("bar"))
-        with lcc.this_dict({"foo": "baz"}):
-            lcc.check_that_entry("foo", lcc.equal_to("baz"))
+        with this_dict({"foo": "bar"}):
+            check_that_entry("foo", equal_to("bar"))
+        with this_dict({"foo": "baz"}):
+            check_that_entry("foo", equal_to("baz"))
 
     test = get_last_test(run_func_in_test(func))
     assert_test_checks(test, expected_successes=2)
@@ -139,12 +139,12 @@ def test_this_dict_multiple():
 
 def test_this_dict_imbricated():
     def func():
-        with lcc.this_dict({"foo": "bar"}):
-            lcc.check_that_entry("foo", lcc.equal_to("bar"))
-            with lcc.this_dict({"foo": "baz"}):
-                lcc.check_that_entry("foo", lcc.equal_to("baz"))
-        with lcc.this_dict({"foo": "foo"}):
-            lcc.check_that_entry("foo", lcc.equal_to("foo"))
+        with this_dict({"foo": "bar"}):
+            check_that_entry("foo", equal_to("bar"))
+            with this_dict({"foo": "baz"}):
+                check_that_entry("foo", equal_to("baz"))
+        with this_dict({"foo": "foo"}):
+            check_that_entry("foo", equal_to("foo"))
 
     test = get_last_test(run_func_in_test(func))
     assert_test_checks(test, expected_successes=3)
@@ -152,33 +152,33 @@ def test_this_dict_imbricated():
 
 def test_this_dict_using_base_key():
     def func():
-        with lcc.this_dict({"foo": {"bar": "baz"}}).using_base_key("foo"):
-            lcc.check_that_entry("bar", lcc.equal_to("baz"))
+        with this_dict({"foo": {"bar": "baz"}}).using_base_key("foo"):
+            check_that_entry("bar", equal_to("baz"))
 
     check = get_last_logged_check(run_func_in_test(func))
 
     assert "foo" in check.description and "bar" in check.description
-    assert check.outcome == True
+    assert check.outcome is True
     assert "baz" in check.details
 
 
 def test_this_dict_using_base_key_as_list():
     def func():
-        with lcc.this_dict({"foo": {"bar": "baz"}}).using_base_key(["foo"]):
-            lcc.check_that_entry("bar", lcc.equal_to("baz"))
+        with this_dict({"foo": {"bar": "baz"}}).using_base_key(["foo"]):
+            check_that_entry("bar", equal_to("baz"))
 
     check = get_last_logged_check(run_func_in_test(func))
 
     assert "foo" in check.description and "bar" in check.description
-    assert check.outcome == True
+    assert check.outcome is True
     assert "baz" in check.details
 
 
 def test_unicode():
     check = get_last_logged_check(
-        run_func_in_test(lambda: lcc.check_that(u"ééé", u"éééààà", lcc.starts_with(u"ééé")))
+        run_func_in_test(lambda: check_that(u"ééé", u"éééààà", starts_with(u"ééé")))
     )
 
     assert u"ééé" in check.description
-    assert check.outcome == True
+    assert check.outcome is True
     assert u"éééààà" in check.details
