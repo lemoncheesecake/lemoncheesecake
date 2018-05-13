cd72fb796466dd470424d28f3122caa245ac5177
Nicolas Delon <nicolas.delon@gmail.com>
2018-05-13 17:35:56 +0200

    example7: improve code readability for documentation

--- examples/example7/suites/add.py
+++ examples/example7/suites/add.py
@@ -1,25 +1,23 @@
 import os.path as osp
-
 import json
 
 import lemoncheesecake.api as lcc
 from lemoncheesecake.matching import *
 
-project_dir = osp.join(osp.dirname(__file__), "..")
-
-
-def test_add(i, j, expected):
-    check_that("%d + %d" % (i, j), i + j, equal_to(expected))
+PROJECT_DIR = osp.join(osp.dirname(__file__), "..")
 
 
 @lcc.suite("Add")
 class add:
     def __init__(self):
-        data = json.load(open(osp.join(project_dir, "data.json")))
+        data = json.load(open(osp.join(PROJECT_DIR, "data.json")))
         for entry in data:
-            lcc.add_test_into_suite(
-                lcc.Test(
-                    entry["name"], entry["description"], lambda: test_add(entry["i"], entry["j"], entry["expected"])
-                ),
-                self
+            test = lcc.Test(
+                entry["name"], entry["description"],
+                lambda: self.test_add(entry["i"], entry["j"], entry["expected"])
             )
+            lcc.add_test_into_suite(test, self)
+
+    @staticmethod
+    def test_add(i, j, expected):
+        check_that("%d + %d" % (i, j), i + j, equal_to(expected))
