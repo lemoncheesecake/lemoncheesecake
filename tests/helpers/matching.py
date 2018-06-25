39546dd64bf515069874fed7dc4cb1b1cb22acda
Nicolas Delon <nicolas.delon@gmail.com>
2018-06-25 21:11:30 +0200

    Simplify matcher unit tests

--- tests/helpers/matching.py
+++ tests/helpers/matching.py
@@ -1,18 +1,11 @@
-import lemoncheesecake.api as lcc
-
-from helpers.runner import run_func_in_test
-from helpers.report import get_last_logged_check
-
-
 def assert_match_result(matcher, actual, result_outcome, result_details):
     result_details_lst = result_details if type(result_details) in (list, tuple) else [result_details]
 
-    check = get_last_logged_check(
-        run_func_in_test(lambda: lcc.check_that("value", actual, matcher))
-    )
-    assert check.outcome == result_outcome
+    result = matcher.matches(actual)
+
+    assert result.outcome == result_outcome
     for details in result_details_lst:
-        assert details in check.details
+        assert details in result.description
 
 
 def assert_match_success(matcher, actual, result_details=()):
--- tests/test_matchers_list.py
+++ tests/test_matchers_list.py
@@ -8,11 +8,11 @@ def test_has_item_success():
 
 
 def test_has_item_failure():
-    assert_match_failure(has_item(greater_than(2)), [1, 2], "No matching")
+    assert_match_failure(has_item(greater_than(2)), [1, 2], "no matching")
 
 
 def test_has_values_success():
-    assert_match_success(has_values([1, 2]), [1, 2, 3], "Got")
+    assert_match_success(has_values([1, 2]), [1, 2, 3], "got")
 
 
 def test_has_values_failure():
@@ -20,7 +20,7 @@ def test_has_values_failure():
 
 
 def test_has_only_values_success():
-    assert_match_success(has_only_values([3, 1, 2]), [1, 2, 3], "Got")
+    assert_match_success(has_only_values([3, 1, 2]), [1, 2, 3], "got")
 
 
 def test_has_only_values_failure_missing():
