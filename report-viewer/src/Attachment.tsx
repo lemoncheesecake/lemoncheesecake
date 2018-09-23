d0b32c3455958587aca06cea68b628d5af6b042b
Nicolas Delon <nicolas.delon@gmail.com>
2018-09-23 18:29:11 +0200

    Support image attachment in report viewer

--- report-viewer/src/Attachment.tsx
+++ report-viewer/src/Attachment.tsx
@@ -8,12 +8,19 @@ interface Props {
 class Attachment extends React.Component<Props, {}> {
     render() {
         const attachment = this.props.attachment;
+        let attached_file;
+
+        if (attachment.as_image) {
+            attached_file = <a href={attachment.filename} target="_blank">{attachment.description}</a>
+        } else {
+            attached_file = <a href={attachment.filename} target="_blank"><img src={attachment.filename} title={attachment.description}/></a>
+        }
 
         return (
             <tr className="step_entry attachment" style={{display: this.props.expanded ? "" : "none"}}>
                 <td className="text-uppercase text-info">ATTACHMENT</td>
                 <td colSpan={3}>
-                    <a href={attachment.filename} target="_blank">{attachment.description}</a>
+                    {attached_file}
                 </td>
             </tr>
         )
