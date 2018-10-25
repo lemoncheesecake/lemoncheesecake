1730b2a5deb7d71c3112ac6ff4b98349d39c44e2
Nicolas Delon <nicolas.delon@gmail.com>
2018-10-25 23:42:24 +0200

    lemoncheesecake.task: handle KeyboardInterrupt through the watchdog system, it allows the abortion of a task that is already scheduled but not yet run

--- lemoncheesecake/task.py
+++ lemoncheesecake/task.py
@@ -1,8 +1,9 @@
-import sys
 from multiprocessing.dummy import Pool, Queue
 
 from lemoncheesecake.exceptions import AbortTask, TasksExecutionFailure, CircularDependencyError, \
     serialize_current_exception
+
+_KEYBOARD_INTERRUPT_ERROR_MESSAGE = "all tests have been interrupted by the user"
 
 
 class BaseTask(object):
@@ -79,19 +80,24 @@ def abort_task(task, context, completed_task_queue, reason=""):
     completed_task_queue.put(task)
 
 
-def abort_tasks(tasks, context, pool, completed_tasks_queue):
+def abort_tasks(tasks, context, pool, completed_tasks_queue, reason=""):
     for task in tasks:
-        pool.apply_async(abort_task, args=(task, context, completed_tasks_queue))
+        pool.apply_async(abort_task, args=(task, context, completed_tasks_queue, reason))
 
 
-def abort_all_tasks(tasks, remaining_tasks, completed_tasks, context, pool, completed_tasks_queue):
-    abort_tasks(remaining_tasks, context, pool, completed_tasks_queue)
+def abort_all_tasks(tasks, remaining_tasks, completed_tasks, context, pool, completed_tasks_queue, reason):
+    abort_tasks(remaining_tasks, context, pool, completed_tasks_queue, reason)
     while len(completed_tasks) != len(tasks):
         completed_task = completed_tasks_queue.get()
         completed_tasks.append(completed_task)
 
 
 def run_tasks(tasks, context=None, nb_threads=1, watchdog=None):
+    got_keyboard_interrupt = False
+    watchdogs = [lambda _: _KEYBOARD_INTERRUPT_ERROR_MESSAGE if got_keyboard_interrupt else None]
+    if watchdog:
+        watchdogs.append(watchdog)
+
     for task in tasks:
         check_task_dependencies(task)
 
@@ -104,7 +110,7 @@ def run_tasks(tasks, context=None, nb_threads=1, watchdog=None):
     try:
         schedule_tasks(
             pop_runnable_tasks(remaining_tasks, completed_tasks, nb_threads),
-            [watchdog] if watchdog else [], context, pool, completed_tasks_queue
+            watchdogs, context, pool, completed_tasks_queue
         )
 
         while len(completed_tasks) != len(tasks):
@@ -115,13 +121,17 @@ def run_tasks(tasks, context=None, nb_threads=1, watchdog=None):
             # schedule next tasks depending on the completed task success
             if completed_task.successful:
                 runnable_tasks = pop_runnable_tasks(remaining_tasks, completed_tasks, nb_threads)
-                schedule_tasks(runnable_tasks, [watchdog] if watchdog else [], context, pool, completed_tasks_queue)
+                schedule_tasks(runnable_tasks, watchdogs, context, pool, completed_tasks_queue)
             else:
                 abortable_tasks = pop_dependant_tasks(completed_task, remaining_tasks)
                 abort_tasks(abortable_tasks, context, pool, completed_tasks_queue)
 
     except KeyboardInterrupt:
-        abort_all_tasks(tasks, remaining_tasks, completed_tasks, context, pool, completed_tasks_queue)
+        got_keyboard_interrupt = True
+        abort_all_tasks(
+            tasks, remaining_tasks, completed_tasks, context, pool, completed_tasks_queue,
+            _KEYBOARD_INTERRUPT_ERROR_MESSAGE
+        )
 
     finally:
         pool.close()
