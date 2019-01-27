acc41eecda450bcfd2ef0c9afba5c735cc497ea5
Nicolas Delon <nicolas.delon@gmail.com>
2019-01-27 23:29:46 +0100

    lcc show: refactor underlying code

--- lemoncheesecake/cli/commands/show.py
+++ lemoncheesecake/cli/commands/show.py
@@ -17,13 +17,56 @@ from lemoncheesecake.project import load_project
 from lemoncheesecake.reporting.console import serialize_metadata
 
 
+class TestTreeRenderer(object):
+    def __init__(self, show_description=False, indent=4):
+        self.show_description = show_description
+        self.indent = indent
+
+    def get_padding(self, depth):
+        return " " * (depth * self.indent)
+
+    def get_test_label(self, test):
+        if self.show_description:
+            return ensure_single_line_text(test.description)
+        else:
+            return test.path
+
+    def get_suite_label(self, suite):
+        if self.show_description:
+            return ensure_single_line_text(suite.description)
+        else:
+            return suite.path
+
+    def show_test(self, test, suite):
+        md = serialize_metadata(test)
+        padding = self.get_padding(suite.hierarchy_depth + 1)
+        test_label = self.get_test_label(test)
+        print("%s- %s%s" % (padding, test_label, " (%s)" % md if md else ""))
+
+    def show_suite(self, suite):
+        md = serialize_metadata(suite)
+        padding = self.get_padding(suite.hierarchy_depth)
+        suite_label = self.get_suite_label(suite)
+        print("%s* %s%s" % (padding, bold(suite_label), " (%s)" % md if md else ""))
+
+        for test in suite.get_tests():
+            self.show_test(test, suite)
+
+        for sub_suite in suite.get_suites():
+            self.show_suite(sub_suite)
+
+    def show_suites(self, suites):
+        for suite in suites:
+            self.show_suite(suite)
+
+
 class ShowCommand(Command):
     def get_name(self):
         return "show"
-    
+
     def get_description(self):
         return "Show the test tree"
-    
+
     def add_cli_args(self, cli_parser):
         add_run_filter_cli_args(cli_parser)
 
@@ -33,49 +76,13 @@ class ShowCommand(Command):
             help="Show suite and test descriptions instead of paths"
         )
 
-    def get_padding(self, depth):
-        return " " * (depth * 4)
-
-    def get_test_label(self, test, show_description):
-        if show_description:
-            return ensure_single_line_text(test.description)
-        else:
-            return test.path
-
-    def get_suite_label(self, suite, show_description):
-        if show_description:
-            return ensure_single_line_text(suite.description)
-        else:
-            return suite.path
-
-    def show_test(self, test, suite, show_description):
-        md = serialize_metadata(test)
-        padding = self.get_padding(suite.hierarchy_depth + 1)
-        test_label = self.get_test_label(test, show_description)
-        print("%s- %s%s" % (padding, test_label, " (%s)" % md if md else ""))
-
-    def show_suite(self, suite, show_description):
-        md = serialize_metadata(suite)
-        padding = self.get_padding(suite.hierarchy_depth)
-        suite_label = self.get_suite_label(suite, show_description)
-        print("%s* %s%s" % (padding, bold(suite_label), " (%s)" % md if md else ""))
-
-        for test in suite.get_tests():
-            self.show_test(test, suite, show_description)
-
-        for sub_suite in suite.get_suites():
-            self.show_suite(sub_suite, show_description)
-
-    def show_suites(self, suites, show_description):
-        for suite in suites:
-            self.show_suite(suite, show_description)
-
     def run_cmd(self, cli_args):
         colorama.init()
 
         project = load_project()
         suites = get_suites_from_project(project, cli_args)
-        
-        self.show_suites(suites, show_description=cli_args.show_description)
+
+        renderer = TestTreeRenderer(show_description=cli_args.show_description)
+        renderer.show_suites(suites)
         
         return 0
