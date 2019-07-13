c92c0e0d6b7ecf3fd92e3d0bd03e2cd8e81a5c9f
Nicolas Delon <nicolas.delon@gmail.com>
2019-07-13 12:11:00 +0200

    doc: remove unused markdown source parser

--- doc/conf.py
+++ doc/conf.py
@@ -43,9 +43,6 @@ autodoc_member_order = "bysource"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
-}
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
