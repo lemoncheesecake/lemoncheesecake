lemoncheesecake/lemoncheesecake

commit a9207f1855e2ad2914a3230dccf2e189a36aa66d

doc: add an API reference section

lemoncheesecake/api.py
@@ -12,5 +12,5 @@ from lemoncheesecake.session import set_step, detached_step, end_step, log_debug
 from lemoncheesecake.fixture import fixture
 from lemoncheesecake.exceptions import AbortTest, AbortSuite, AbortAllTests, UserError
 
-# for pydoc
+# for pydoc & sphinx
 __all__ = dir()

lemoncheesecake/matching/__init__.py
@@ -2,3 +2,6 @@ from .matchers import *
 from .operations import check_that, require_that, assert_that, check_that_in, require_that_in, assert_that_in
 
 DISPLAY_DETAILS_WHEN_EQUAL = True
+
+# for pydoc & sphinx
+__all__ = dir()

doc/conf.py
@@ -37,6 +37,9 @@ from lemoncheesecake import __version__
 # ones.
 extensions = ["sphinx.ext.autodoc"]
 
+# Autodoc config:
+autodoc_member_order = "bysource"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

lemoncheesecake/exceptions.py
@@ -1,9 +1,3 @@
-'''
-Created on Sep 8, 2016
-
-@author: nicolas
-'''
-
 from typing import Optional
 
 import sys
@@ -53,6 +47,10 @@ class UnknownReportBackendError(LemoncheesecakeException):
 
 
 class AbortTest(LemoncheesecakeException):
+    """
+    Raising this exception will stop the currently running test.
+    """
+
     message_prefix = "The test has been aborted"
 
     def __init__(self, reason):
@@ -60,6 +58,9 @@ class AbortTest(LemoncheesecakeException):
 
 
 class AbortSuite(LemoncheesecakeException):
+    """
+    Raising this exception will stop the currently running suite.
+    """
     message_prefix = "The suite has been aborted"
 
     def __init__(self, reason):
@@ -67,6 +68,9 @@ class AbortSuite(LemoncheesecakeException):
 
 
 class AbortAllTests(LemoncheesecakeException):
+    """
+    Raising this exception will stop the currently running test and all the tests waiting to be run.
+    """
     message_prefix = "All tests have been aborted"
 
     def __init__(self, reason):
@@ -74,7 +78,13 @@ class AbortAllTests(LemoncheesecakeException):
 
 
 class UserError(LemoncheesecakeException):
-    pass
+    """
+    This exception is intended to be raised in pre-run and post-run phases of the project
+    to indicate that a required state has not been fulfilled.
+    """
+    # NB: sphinx requires the constructor to be overriden, otherwise it raises an error
+    def __init__(self, reason):
+        LemoncheesecakeException.__init__(self, reason)
 
 
 class InvalidReportFile(LemoncheesecakeException):

lemoncheesecake/fixture.py
@@ -1,11 +1,6 @@
-'''
-Created on Jan 7, 2017
-
-@author: nicolas
-'''
-
 import inspect
 from collections import OrderedDict
+from typing import List, Any, Sequence, Callable
 
 from lemoncheesecake.helpers.moduleimport import import_module, get_matching_files, get_py_files_from_dir
 from lemoncheesecake.exceptions import FixtureError, ProgrammingError
@@ -29,6 +24,12 @@ class _FixtureInfo:
 
 
 def fixture(names=None, scope="test"):
+    """
+    Decorator. Declare a function as a fixture.
+    :param names: an optional list of names that can be used to access the fixture value,
+        if no names are provided the decorated function name will be used
+    :param scope: the fixture scope, available scopes are: "test", "suite", "session", "pre_run"; default is "test"
+    """
     def wrapper(func):
         if scope not in _SCOPE_LEVELS.keys():
             raise ProgrammingError("Invalid fixture scope '%s' in fixture function '%s'" % (scope, func.__name__))
@@ -298,6 +299,10 @@ class FixtureRegistry:
 
 
 def load_fixtures_from_func(func):
+    # type: (Callable) -> List[Fixture]
+    """
+    Load a fixture from a function.
+    """
     assert hasattr(func, "_lccfixtureinfo")
     names = func._lccfixtureinfo.names
     if not names:
@@ -308,6 +313,10 @@ def load_fixtures_from_func(func):
 
 
 def load_fixtures_from_file(filename):
+    # type: (str) -> List[Fixture]
+    """
+    Load fixtures from a given file.
+    """
     mod = import_module(filename)
 
     fixtures = []
@@ -320,13 +329,18 @@ def load_fixtures_from_file(filename):
 
 
 def load_fixtures_from_files(patterns, excluding=[]):
+    # type: (Any[str, Sequence[str]], Any[str, Sequence[str]]) -> List[Fixture]
     """
-    Import fixtures from a list of files:
-    - patterns: a mandatory list (a simple string can also be used instead of a single element list)
-      of files to import; the wildcard '*' character can be used
-    - exclude: an optional list (a simple string can also be used instead of a single element list)
+    Load fixtures from files.
+
+    :param patterns: a mandatory list (a simple string can also be used instead of a single element list)
+        of files to import; the wildcard '*' character can be used
+    :param exclude: an optional list (a simple string can also be used instead of a single element list)
       of elements to exclude from the expanded list of files to import
-    Example: load_suites_from_files("test_*.py")
+
+    Example::
+
+        load_suites_from_files("test_*.py")
     """
     fixtures = []
     for file in get_matching_files(patterns, excluding):
@@ -335,6 +349,10 @@ def load_fixtures_from_files(patterns, excluding=[]):
 
 
 def load_fixtures_from_directory(dir):
+    # type: (str) -> List[Fixture]
+    """
+    Load fixtures from a given directory (not recursive).
+    """
     fixtures = []
     for file in get_py_files_from_dir(dir):
         fixtures.extend(load_fixtures_from_file(file))

lemoncheesecake/project.py
@@ -26,26 +26,43 @@ DEFAULT_REPORTING_BACKENDS = ("console", "json", "html")
 
 class Project(object):
     def __init__(self, project_dir):
+        #: The project's directory path
         self.dir = project_dir  # type: str
+        #: The project's metadata policy
         self.metadata_policy = MetadataPolicy()
+        #: Indicates whether or not the project supports parallel execution of tests
         self.threaded = True  # type: bool
+        #: The reporting backends of the project as a dict (whose key is the reporting backend name)
         self.reporting_backends = {b.get_name(): b for b in get_reporting_backends()}  # type: Dict[str, ReportingBackend]
+        #: The list of default reporting backend (indicated by their name) that will be used by "lcc run"
         self.default_reporting_backend_names = list(DEFAULT_REPORTING_BACKENDS)
 
     def add_cli_args(self, cli_parser):
         # type: (argparse.ArgumentParser) -> None
+        """
+        Overridable. This method can be used to add extra CLI arguments to "lcc run".
+        """
         pass
 
     def create_report_dir(self):
         # type: () -> str
+        """
+        Overridable. Create the report directory when no report directory is specified to "lcc run".
+        """
         return create_report_dir_with_rotation(self.dir)
 
     def load_suites(self):
         # type: () -> Sequence[Suite]
+        """
+        Overridable. Load the project's suites.
+        """
         return load_suites_from_directory(osp.join(self.dir, "suites"))
 
     def load_fixtures(self):
         # type: () -> Sequence[Fixture]
+        """
+        Overridable. Load the project's fixtures.
+        """
         fixtures_dir = osp.join(self.dir, "fixtures")
         if osp.exists(fixtures_dir):
             return load_fixtures_from_directory(fixtures_dir)
@@ -54,18 +71,38 @@ class Project(object):
 
     def pre_run(self, cli_args, report_dir):
         # type: (Any, str) -> None
+        """
+        Overridable. This hook is called before running the tests.
+        """
         pass
 
     def post_run(self, cli_args, report_dir):
         # type: (Any, str) -> None
+        """
+        Overridable. This hook is called after running the tests.
+        """
         pass
 
     def build_report_title(self):
         # type: () -> Optional[str]
+        """
+        Overridable. Build a custom report title as a string.
+        """
         return None
 
     def build_report_info(self):
         # type: () -> Sequence[Tuple[str, str]]
+        """
+        Overridable. Build a list key/value pairs (expressed as a two items tuple)
+        that will be available in the report.
+
+        Example::
+
+            [
+                ("key1", "value1"),
+                ("key2", "value2")
+            ]
+        """
         return []
 
 

lemoncheesecake/session.py
@@ -118,8 +118,12 @@ def get_report():
 
 
 def set_step(description, detached=False):
+    # type: (str, bool) -> None
     """
     Set a new step.
+
+    :param description: the step description
+    :param detached: whether or not the step is "detached"
     """
     get_session().set_step(description, detached=detached)
 
@@ -133,6 +137,10 @@ def end_step(step):
 
 @contextmanager
 def detached_step(description):
+    """
+    Context manager. Like set_step, but ends the step at the end of the "with" block.
+    Intended to be use with code run through lcc.Thread.
+    """
     set_step(description, detached=True)
     try:
         yield
@@ -186,8 +194,8 @@ def _prepare_attachment(filename, description=None, as_image=False):
 
 def prepare_attachment(filename, description=None):
     """
-    Prepare a attachment using a pseudo filename and an optional description.
-    The function returns the real filename on disk that will be used by the caller
+    Context manager. Prepare a attachment using a pseudo filename and an optional description.
+    It returns the real filename on disk that will be used by the caller
     to write the attachment content.
     """
     return _prepare_attachment(filename, description)
@@ -195,7 +203,7 @@ def prepare_attachment(filename, description=None):
 
 def prepare_image_attachment(filename, description=None):
     """
-    Prepare an image attachment using a pseudo filename and an optional description.
+    Context manager. Prepare an image attachment using a pseudo filename and an optional description.
     The function returns the real filename on disk that will be used by the caller
     to write the attachment content.
     """
@@ -297,6 +305,10 @@ def is_session_successful():
 
 
 class Thread(threading.Thread):
+    """
+    Acts exactly as the standard threading.Thread class and must be used instead when running threads
+    within a test.
+    """
     def __init__(self, *args, **kwargs):
         super(Thread, self).__init__(*args, **kwargs)
         self.location = get_session().location

commit 06a786f0d77a0a10c2175b8af3d9ed8aa65b8304

Fix import related issue introduced in a9207f1

lemoncheesecake/api.py
@@ -13,4 +13,4 @@ from lemoncheesecake.fixture import fixture
 from lemoncheesecake.exceptions import AbortTest, AbortSuite, AbortAllTests, UserError
 
 # for pydoc & sphinx
-__all__ = dir()
+__all__ = [sym_name for sym_name in dir() if not sym_name.startswith("_")]

lemoncheesecake/matching/__init__.py
@@ -4,4 +4,4 @@ from .operations import check_that, require_that, assert_that, check_that_in, re
 DISPLAY_DETAILS_WHEN_EQUAL = True
 
 # for pydoc & sphinx
-__all__ = dir()
+__all__ = [sym_name for sym_name in dir() if not sym_name.startswith("_")]