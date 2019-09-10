26ed458da4993868991676d139616e1200c9a6ed
Nicolas Delon <nicolas.delon@gmail.com>
2019-09-10 15:08:04 +0200

    lcc report: improve broken pipe handling on Python 3

--- lemoncheesecake/cli/commands/report.py
+++ lemoncheesecake/cli/commands/report.py
@@ -1,4 +1,6 @@
 import sys
+import errno
+from contextlib import contextmanager
 
 from lemoncheesecake.cli.command import Command
 from lemoncheesecake.cli.utils import auto_detect_reporting_backends, add_report_path_cli_arg, get_report_path
@@ -6,6 +8,25 @@ from lemoncheesecake.reporting import load_report
 from lemoncheesecake.reporting.backends.console import print_report_as_test_run
 from lemoncheesecake.reporting.console import print_report
 from lemoncheesecake.filter import add_result_filter_cli_args, make_result_filter
+
+
+@contextmanager
+def ignore_broken_pipe():
+    try:
+        yield
+    # catch IOError + check errno (instead of simply catching BrokenPipeError on Python 3)
+    # to be Python 2 compatible
+    except IOError as excp:
+        if excp.errno == errno.EPIPE:
+            # Broken pipe (example: "lcc report | head"), in that case, simply exit gracefully
+            # We just close stderr to avoid Python warnings about ignored exception at the end of the
+            # command. This behavior occurs with Python3 (not 2), see :
+            # https://stackoverflow.com/questions/26692284/how-to-prevent-brokenpipeerror-when-doing-a-flush-in-python
+            # please note that the ignored exception is related to the stream wrapper of colorama
+            sys.stderr.close()
+        else:
+            # otherwise, re-raise
+            raise
 
 
 class ReportCommand(Command):
@@ -42,18 +63,10 @@ class ReportCommand(Command):
         if cli_args.short:
             print_report_as_test_run(report, result_filter)
         else:
-            try:
+            with ignore_broken_pipe():
                 print_report(
                     report, result_filter=result_filter, max_width=cli_args.max_width,
                     explicit=cli_args.explicit or not sys.stdout.isatty()
                 )
-            except IOError as excp:
-                if excp.errno == 32:
-                    # broken pipe (example: "lcc report | less" and the user quit less)
-                    # in that case, simply exit gracefully
-                    pass
-                else:
-                    # otherwise, re-raise
-                    raise
 
         return 0
