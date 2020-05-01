eba51ad79cc8449ff9c6294a245f95d213fd2d27
Nicolas Delon <nicolas.delon@gmail.com>
2020-05-01 19:48:42 +0200

    Fix unexpected extra symbols import when doing the officially documented 'from lemoncheesecake.matching import *'

--- lemoncheesecake/matching/__init__.py
+++ lemoncheesecake/matching/__init__.py
@@ -1,7 +1,17 @@
+import inspect
+
 from .matchers import *
 from .operations import check_that, require_that, assert_that, check_that_in, require_that_in, assert_that_in
 
 DISPLAY_DETAILS_WHEN_EQUAL = True
 
-# for pydoc & sphinx
-__all__ = [sym_name for sym_name in dir() if not sym_name.startswith("_")]
+
+def _is_symbol_exportable(sym_name):
+    return all((
+        sym_name != "DISPLAY_DETAILS_WHEN_EQUAL",
+        not sym_name.startswith("_"),
+        not inspect.ismodule(globals()[sym_name])
+    ))
+
+
+__all__ = list(filter(_is_symbol_exportable, dir()))
