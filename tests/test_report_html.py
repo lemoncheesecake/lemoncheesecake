aff177b03ea30c3d9341a80eebd04d942695520d
Nicolas Delon <nicolas.delon@gmail.com>
2022-01-06 23:05:48 +0100

    html backend: update script to fit changes of the report viewer where CSS is now part of the build; there is no longer static VS non-static mode, it's now full static (no external resources are used)

--- tests/test_report_html.py
+++ tests/test_report_html.py
@@ -1,5 +1,4 @@
 import os.path as osp
-import glob
 
 import lemoncheesecake.api as lcc
 from lemoncheesecake.reporting.backends import HtmlBackend
@@ -7,22 +6,20 @@ from lemoncheesecake.reporting.backends import HtmlBackend
 from helpers.runner import run_suite_class
 
 
-@lcc.suite("suite")
-class Suite(object):
-    @lcc.test("test")
-    def test(self):
-        lcc.log_info("something")
+def test_html(tmpdir):
+    @lcc.suite()
+    class Suite(object):
+        @lcc.test()
+        def test(self):
+            lcc.log_info("something")
 
-
-def test_html_fat(tmpdir):
     run_suite_class(Suite, backends=(HtmlBackend(),), tmpdir=tmpdir)
-    assert osp.exists(osp.join(tmpdir.strpath, "report.html"))
-    assert glob.glob(osp.join(tmpdir.strpath, ".html", "bootstrap*"))
 
-
-def test_html_non_fat(tmpdir):
-    backend = HtmlBackend()
-    backend.fat_html = False
-    run_suite_class(Suite, backends=(backend,), tmpdir=tmpdir)
-    assert osp.exists(osp.join(tmpdir.strpath, "report.html"))
-    assert not glob.glob(osp.join(tmpdir.strpath, ".html", "bootstrap*"))
+    expected = (
+        "report.html",
+        osp.join(".html", "report.js"),
+        osp.join(".html", "report.css"),
+        osp.join(".html", "logo.png")
+    )
+    for path in expected:
+        assert osp.exists(osp.join(tmpdir.strpath, path))
