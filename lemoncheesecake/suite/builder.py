5a70268d827bd6d0e1a7fe290e5ce1f1303312df
Nicolas Delon <nicolas.delon@gmail.com>
2022-12-04 22:05:53 +0100

    doc: update documentation for lcc.depends_on callable arguments

--- lemoncheesecake/suite/builder.py
+++ lemoncheesecake/suite/builder.py
@@ -168,7 +168,17 @@ def depends_on(*deps: Union[str, Callable]) -> Any:
     """
     Decorator, only applicable to a test. Add dependencies to a test.
 
-    :param deps: the test paths that the decorated test is depending on.
+    :param deps: dependencies can be either:
+
+        - a full test path::
+
+            @lcc.depends("mysuite.mytest")
+
+        - a callable that express dependencies as a filter (a :py:class:`Test <lemoncheesecake.api.Test>` instance is passed to the callable)::
+
+            @lcc.depends(lambda test: "mytag" in test.tags)
+
+        .. versionadded:: 1.15.0 callable syntax
     """
     def wrapper(obj):
         md = get_metadata(obj)
