lemoncheesecake/lemoncheesecake

commit 547f8fd772f226d173fa1203b8f61fef1390ee8b

Fix for lcc run --save-report every_Ns: compute the time interval using the time the report has been actually saved + improve some naming

lemoncheesecake/reporting/backend.py
@@ -5,6 +5,7 @@ Created on Mar 29, 2016
 '''
 
 import os
+import time
 
 from lemoncheesecake.helpers.orderedset import OrderedSet
 from lemoncheesecake.exceptions import LemoncheesecakeException
@@ -40,17 +41,19 @@ class ReportingBackend:
 
 
 class FileReportSession(ReportingSession):
-    def __init__(self, report_filename, report, reporting_backend, report_saving_strategy):
-        self.report_filename = report_filename
+    def __init__(self, path, report, backend, saving_strategy):
+        self.path = path
         self.report = report
-        self.reporting_backend = reporting_backend
-        self.report_saving_strategy = report_saving_strategy
+        self.backend = backend
+        self.saving_strategy = saving_strategy
+        self.last_saved_time = None
 
     def _save(self):
-        self.reporting_backend.save_report(self.report_filename, self.report)
+        self.backend.save_report(self.path, self.report)
+        self.last_saved_time = time.time()
 
     def _handle_event(self, event):
-        report_must_be_saved = self.report_saving_strategy and self.report_saving_strategy(event, self.report)
+        report_must_be_saved = self.saving_strategy and self.saving_strategy(event, self.report, self.last_saved_time)
         if report_must_be_saved:
             self._save()
 

lemoncheesecake/reporting/savingstrategy.py
@@ -27,15 +27,15 @@ def _is_end_of_result_event(event):
     return None
 
 
-def save_at_each_suite_strategy(event, _):
+def save_at_each_suite_strategy(event, _, __):
     return isinstance(event, SuiteEndEvent)
 
 
-def save_at_each_test_strategy(event, _):
+def save_at_each_test_strategy(event, _, __):
     return _is_end_of_result_event(event) is not None
 
 
-def save_at_each_failed_test_strategy(event, report):
+def save_at_each_failed_test_strategy(event, report, _):
     location = _is_end_of_result_event(event)
     if location:
         result = report.get(location)
@@ -44,25 +44,19 @@ def save_at_each_failed_test_strategy(event, report):
         return False
 
 
-def save_at_each_log_strategy(event, _):
+def save_at_each_log_strategy(event, _, __):
     return isinstance(event, SteppedEvent)
 
 
 class SaveAtInterval:
     def __init__(self, interval):
         self.interval = interval
-        self.last_saving = None
 
-    def __call__(self, event, report):
-        now = time.time()
-        if self.last_saving:
-            must_be_saved = now > self.last_saving + self.interval
-            if must_be_saved:
-                self.last_saving = now
-            return must_be_saved
-        else:
-            self.last_saving = now  # not a saving but an initialization
+    def __call__(self, event, report, last_saved_time):
+        if last_saved_time is None:
             return False
+
+        return last_saved_time + self.interval < time.time()
 
 
 def make_report_saving_strategy(expression):

commit 78afc4d4ee093d66a1ecab0cba122a0702c27619

Fix --save-report every_Ns no longer saving anything (bug introduced in 547f8fd)

lemoncheesecake/reporting/backend.py
@@ -46,7 +46,7 @@ class FileReportSession(ReportingSession):
         self.report = report
         self.backend = backend
         self.saving_strategy = saving_strategy
-        self.last_saved_time = None
+        self.last_saved_time = time.time()
 
     def _save(self):
         self.backend.save_report(self.path, self.report)

lemoncheesecake/reporting/savingstrategy.py
@@ -53,9 +53,6 @@ class SaveAtInterval:
         self.interval = interval
 
     def __call__(self, event, report, last_saved_time):
-        if last_saved_time is None:
-            return False
-
         return last_saved_time + self.interval < time.time()
 
 